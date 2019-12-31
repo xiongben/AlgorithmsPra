ef999a207da17b5dfd74338a6a605be436a220eb
Xiong Ben <xiongben1993@gmail.com>
2019-12-31 23:51:07 +0800

    [add]

---
+++ src/main/java/com/xiongben/linkedList/Josepfu.java
@@ -0,0 +1,50 @@
+package com.xiongben.linkedList;
+
+public class Josepfu {
+    public static void main(String[] args) {
+
+    }
+}
+
+class CircleSingleLinkedList {
+
+    private Boy first = null;
+
+    public void addBoy(int nums){
+
+    }
+
+    public void showBoy() {
+
+    }
+
+    public void countBoy(int startNo,int countNum,int nums){
+
+    }
+}
+
+class Boy {
+
+    private int no;
+    private Boy next;
+
+    Boy(int no){
+        this.no = no;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public Boy getNext() {
+        return next;
+    }
+
+    public void setNext(Boy next) {
+        this.next = next;
+    }
+}
