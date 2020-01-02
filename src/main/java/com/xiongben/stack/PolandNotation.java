46c3a2a015a0fd4ffc546d5c002ff140f84e6279
xiongben <xiongben1993@gmail.com>
2020-01-02 17:57:21 +0800

    [add] PolandNotation

--- src/main/java/com/xiongben/stack/PolandNotation.java
+++ src/main/java/com/xiongben/stack/PolandNotation.java
@@ -1,7 +1,117 @@
 package com.xiongben.stack;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class PolandNotation {
     public static void main(String[] args) {
+        String expression = "1+((2+3)*4)-5";//注意表达式
+        List<String> infixExpressionList = toInfixExpressionList(expression);
+        System.out.println("中缀表达式对应的List=" + infixExpressionList); // ArrayList [1,+,(,(,2,+,3,),*,4,),-,5]
+        List<String> suffixExpreesionList = parseSuffixExpressionList(infixExpressionList);
+        System.out.println("后缀表达式对应的List" + suffixExpreesionList); //ArrayList [1,2,3,+,4,*,+,5,–]
 
+        System.out.printf("expression=%d", calculate(suffixExpreesionList));
+
+    }
+
+    public static List<String> parseSuffixExpressionList(List<String> ls){
+        Stack<String> s1 = new Stack<String>();
+        List<String> s2 = new ArrayList<String>();
+        for (String item : ls){
+            if(item.matches("\\d+")){
+                s2.add(item);
+            }else if(item.equals("(")){
+                s1.push(item);
+            }else if(item.equals(")")){
+                while (!s1.peek().equals("(")){
+                    s2.add(s1.pop());
+                }
+                s1.pop();//将（弹出，消除小括号
+            }else {
+                //当item的优先级小于等于s1栈顶运算符, 将s1栈顶的运算符弹出并加入到s2中，再次转到(4.1)与s1中新的栈顶运算符相比较
+                while (s1.size() != 0 && Operation.getValue(s1.peek()) >= Operation.getValue(item)){
+                    s2.add(s1.pop());
+                }
+                s1.push(item);
+            }
+        }
+        while (s1.size() != 0){
+            s2.add(s1.pop());
+        }
+        return s2;
+    }
+
+    public static List<String> toInfixExpressionList(String s) {
+        List<String> ls = new ArrayList<String>();
+        int i = 0;
+        String str;
+        char c;
+        do {
+            c = s.charAt(i);
+            if(c < 48 || c > 57) {
+                ls.add(""+c);
+                i++;
+            }else {
+                str = "";
+                if(c <= 57 && c >= 48 && i < s.length()){
+                    str += c;
+                    i++;
+                }
+                ls.add(str);
+            }
+        }while (i < s.length());
+        return ls;
+    }
+
+    public static int calculate(List<String> ls){
+        Stack<String> stack = new Stack<String>();
+        for (String item : ls){
+            if(item.matches("\\d+")){
+                stack.push(item);
+            }else {
+                int num2 = Integer.parseInt(stack.pop());
+                int num1 = Integer.parseInt(stack.pop());
+                int res = 0;
+                if (item.equals("+")) {
+                    res = num1 + num2;
+                } else if (item.equals("-")) {
+                    res = num1 - num2;
+                } else if (item.equals("*")) {
+                    res = num1 * num2;
+                } else if (item.equals("/")) {
+                    res = num1 / num2;
+                } else {
+                    throw new RuntimeException("运算符有误");
+                }
+                stack.push(""+res);
+            }
+        }
+        return Integer.parseInt(stack.pop());
+    }
+
+}
+
+class Operation {
+    private static int ADD = 1;
+    private static int SUB = 1;
+    private static int MUL = 2;
+    private static int DIV = 2;
+
+    public static int getValue(String operation){
+        int res = 0;
+        if ("+".equals(operation)) {
+            res = ADD;
+        } else if ("-".equals(operation)) {
+            res = SUB;
+        } else if ("*".equals(operation)) {
+            res = MUL;
+        } else if ("/".equals(operation)) {
+            res = DIV;
+        } else {
+            System.out.printf("不支持该运算符:%s\n",operation);
+        }
+        return res;
     }
 }
