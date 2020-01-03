75b68aaabfaca6ff6f0d3dc58b9ce2f42b631f15
xiongben <xiongben1993@gmail.com>
2020-01-03 15:59:01 +0800

    [add] QuickSort

--- src/main/java/com/xiongben/sort/InsertSort.java
+++ src/main/java/com/xiongben/sort/InsertSort.java
@@ -20,13 +20,13 @@ public class InsertSort {
         int insertIndex = 0;
         for (int i=1;i<arr.length;i++){
             insertVal = arr[i];
-            insertIndex = i-1;
-            while (insertIndex >= 0 && insertVal<arr[insertIndex]){
-                arr[insertIndex+1] = arr[insertIndex];
+            insertIndex = i;
+            while (insertIndex > 0 && insertVal<arr[insertIndex-1]){
+                arr[insertIndex] = arr[insertIndex-1];
                 insertIndex--;
             }
-            if(insertIndex+1 != i){
-                arr[insertIndex+1] = insertVal;
+            if(insertIndex != i){
+                arr[insertIndex] = insertVal;
             }
         }
     }
--- src/main/java/com/xiongben/sort/ShellSort.java
+++ src/main/java/com/xiongben/sort/ShellSort.java
@@ -1,4 +1,13 @@
 package com.xiongben.sort;
 
 public class ShellSort {
+    public static void main(String[] args) {
+        for (int i=2;i<10;i++){
+            System.out.println("====="+(i-2)+"=====");
+            for (int j=i-2;j>=0;j-=2){
+                System.out.println(j);
+            }
+            System.out.println("==========");
+        }
+    }
 }
