c169b122fdc1e0c6e8ec7b34eb82bbb0c9c400de
Xiong Ben <xiongben1993@gmail.com>
2020-01-14 23:31:09 +0800

    [add]

--- src/main/java/com/xiongben/huffmancode/HuffmanCode.java
+++ src/main/java/com/xiongben/huffmancode/HuffmanCode.java
@@ -7,6 +7,31 @@ public class HuffmanCode {
 
     }
 
+    //生成霍夫曼树对应的霍夫曼编码
+    static Map<Byte, String> huffmanCodes = new HashMap<Byte, String>();
+    static StringBuilder stringBuilder = new StringBuilder();
+
+    /**
+     *
+     * @param node 传入结点
+     * @param code  路径 ，左子节点是0，右子节点是1
+     * @param stringBuilder 用于字符串拼接
+     */
+    private static void getCodes(Node node,String code,StringBuilder stringBuilder){
+        StringBuilder stringBuilder2 = new StringBuilder(stringBuilder);
+        stringBuilder2.append(code);
+        if(node != null){
+            if(node.data == null){
+                getCodes(node.left,"0",stringBuilder2);
+                getCodes(node.right,"1",stringBuilder2);
+            }else {
+                huffmanCodes.put(node.data,stringBuilder2.toString());
+            }
+        }
+
+    }
+
+
     //前序遍历
     private static void preOrder(Node root){
         if(root != null){
@@ -30,6 +55,22 @@ public class HuffmanCode {
         for (Map.Entry<Byte,Integer> entry:counts.entrySet()){
             nodes.add(new Node(entry.getKey(),entry.getValue()));
         }
+        return nodes;
+    }
+
+    private static Node createHuffmanTree(List<Node> nodes){
+        while (nodes.size()>1){
+            Collections.sort(nodes);
+            Node leftNode = nodes.get(0);
+            Node rightNode = nodes.get(1);
+            Node parent = new Node(null,leftNode.weight+rightNode.weight);
+            parent.left = leftNode;
+            parent.right = rightNode;
+            nodes.remove(leftNode);
+            nodes.remove(rightNode);
+            nodes.add(parent);
+        }
+        return nodes.get(0);
     }
 
 }
