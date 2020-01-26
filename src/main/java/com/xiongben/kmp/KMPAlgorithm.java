b270eba7bddd26cf909a05577274abaf0c3caddf
Xiong Ben <xiongben1993@gmail.com>
2020-01-26 22:46:49 +0800

    [add]

---
+++ src/main/java/com/xiongben/kmp/KMPAlgorithm.java
@@ -0,0 +1,10 @@
+package com.xiongben.kmp;
+
+public class KMPAlgorithm {
+    public static void main(String[] args) {
+        String str1 = "硅硅谷 尚硅谷你尚硅 尚硅谷你尚硅谷你尚硅你好";
+        String str2 = "尚硅谷你尚硅你~";
+    }
+
+
+}
