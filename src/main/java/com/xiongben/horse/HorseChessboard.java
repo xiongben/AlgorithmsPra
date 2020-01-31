7b2dd4ea6d38b6988de51c986066ea5196bfa6b8
Xiong Ben <xiongben1993@gmail.com>
2020-01-31 22:12:38 +0800

    [add]

--- src/main/java/com/xiongben/horse/HorseChessboard.java
+++ src/main/java/com/xiongben/horse/HorseChessboard.java
@@ -1,7 +1,29 @@
 package com.xiongben.horse;
 
+import java.awt.*;
+import java.util.ArrayList;
+
 public class HorseChessboard {
+
+    private static int X;
+    private static int Y;
+    private static boolean visited[];
+    private static boolean finished;
+
+
     public static void main(String[] args) {
 
+
+    }
+
+    /**
+     * 根据当前位置，计算马儿还能走到哪些位置，并放入到一个集合中，最多有8个位置
+     * @param curPoint
+     * @return
+     */
+    public static ArrayList<Point> next(Point curPoint) {
+        ArrayList<Point> ps = new ArrayList<Point>();
+        Point p1 = new Point();
+        
     }
 }
